1a8d49a767b8389a36b1aeacfae809cb1d71503f
Sidney Muriano <smuriano@gmail.com>
2020-05-29 17:10:25 -0300

    Ajuste no docker-compose e dockerfile

--- src/MavveErp.Api/Startup.cs
+++ src/MavveErp.Api/Startup.cs
@@ -122,10 +122,7 @@ namespace MavveErp.Api
       {
         app.UseExceptionHandler("/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
       }
-
-      app.UseHttpsRedirection();
 
       app.UseCors(x => x
         .AllowAnyOrigin()
